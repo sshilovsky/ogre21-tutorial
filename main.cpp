cff809ff82a54447237e29866b797e5159076985
Sergei Shilovsky <sshilovsky@gmail.com>
2017-06-20 10:15:24 +0300

    window, scene and threading support (with boost)

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-
 #include <OgreRoot.h>
 
 int main() {
     Ogre::Root root;
-    std::cout << "Hello, World!" << std::endl;
+    if (!root.restoreConfig() && !root.showConfigDialog()) {
+        return 2;
+    }
+    root.initialise(false);
+    auto win = root.createRenderWindow("test", 1024, 720, false);
+    auto scnMgr = root.createSceneManager(Ogre::ST_GENERIC, 2, Ogre::INSTANCING_CULLING_THREADED);
+
+    root.startRendering();
+
     return 0;
 }
